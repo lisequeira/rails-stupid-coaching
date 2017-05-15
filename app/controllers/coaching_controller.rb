0440a1aea1fb9a5b7029c84281cbc9a8f8ef4d3f
Liliana Sequeira <lilianasrsequeira@gmail.com>
2017-05-15 11:09:22 +0100

    stupid coach with answers

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,7 +1,26 @@
 class CoachingController < ApplicationController
   def answer
+    @question = params[:query]
+
+    @answer = coach_answer_enhanced(@question)
   end
 
   def ask
+
   end
+
+
+  private
+
+  def coach_answer_enhanced(your_message)
+    if your_message.include? "?"
+      answer = "Silly question, get dressed and go to work!"
+    else
+      answer = "I don't care, get dressed and go to work!"
+    end
+
+    return answer
+
+  end
+
 end
